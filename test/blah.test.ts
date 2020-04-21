2df17d865cdd0bc2756131c7da9535f3a8b28a6f
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-04-22 05:02:09 +0800

    Add difference function

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,10 @@
-import { sum } from '../src';
+import { sum, difference } from '../src';
 
 describe('Math', () => {
   it('Sum', () => {
     expect(sum(12, 200, 120)).toEqual(332);
+  });
+  it('Difference', () => {
+    expect(difference(120, 200, -40)).toEqual(-40);
   });
 });
