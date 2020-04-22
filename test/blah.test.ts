729d5b972d67ef054dc840afaef8dde4138b5c4e
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-04-22 14:53:30 +0800

    Add product function

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,4 +1,4 @@
-import { sum, difference } from '../src';
+import { sum, difference, product } from '../src';
 
 describe('Math', () => {
   it('Sum', () => {
@@ -6,5 +6,8 @@ describe('Math', () => {
   });
   it('Difference', () => {
     expect(difference(120, 200, -40)).toEqual(-40);
+  });
+  it('Product', () => {
+    expect(product(-2, -4, 6)).toEqual(48);
   });
 });
