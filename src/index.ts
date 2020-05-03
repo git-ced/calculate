b4ac433ff58784696b3c0f3fb6332c0b5488c189
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-05-04 02:35:10 +0800

    Add sqaure function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+// ANCHOR Math
 export { default as sum } from './sum';
 export { default as difference } from './difference';
 export { default as product } from './product';
 export { default as quotient } from './quotient';
 export { default as remainder } from './remainder';
+export { default as square } from './sqaure';
+// ANCHOR Statistics
 export { default as mean } from './mean';
 export { default as median } from './median';
 export { default as mode } from './mode';
 export { default as range } from './range';
+// ANCHOR Sort
 export { default as sort } from './sort';
 export { default as insertionSort } from './insertionSort';
 export { default as quicksort } from './quicksort';
---
+++ src/sqaure/index.ts
@@ -0,0 +1,17 @@
+/**
+ * @name square
+ * @summary Gets the square of a number.
+ *
+ * @description Gets the square of a number.
+ *
+ * @example
+ * // Normal usage
+ * let result = sqaure(4);
+ * // => 16
+ *
+ * @param {numberr} a - number to be squared.
+ * @returns {number}
+ */
+export default function square(a: number): number {
+  return a * a;
+}
